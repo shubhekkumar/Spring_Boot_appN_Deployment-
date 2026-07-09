0326b8bcb9e95799c4d690496f8bc663af1484b7
shubhekkumar <shubhekkumar@gmail.com>
2026-07-09 16:26:01 +0530

    feat(exception): add global exception handling and dto files

---
+++ src/main/java/com/taskmaster/exception/TaskNotFoundException.java
@@ -0,0 +1,9 @@
+package com.taskmaster.exception;
+
+public class TaskNotFoundException extends RuntimeException {
+
+    public TaskNotFoundException(String message) {
+        super(message);
+    }
+
+}
