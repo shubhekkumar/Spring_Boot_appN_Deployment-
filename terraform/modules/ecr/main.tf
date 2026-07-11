06e294032cfb4e2229333a68a76b61a6aac06307
shubhekkumar <shubhekkumar@gmail.com>
2026-07-11 16:06:01 +0530

    style(terraform): format terraform files

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -2,10 +2,10 @@ resource "aws_ecr_repository" "taskmaster" {
 
   name = "${var.project_name}-${var.environment}"
 
-  force_delete = true  
+  force_delete = true
 
   image_tag_mutability = "MUTABLE"
-  
+
   image_scanning_configuration {
     scan_on_push = true
   }
